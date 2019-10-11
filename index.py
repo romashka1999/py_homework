13845e89e61b0cd25e69ffc3eeddaac5a50cdb95
Roma <r.chikhladze@adjarabet.com>
2019-10-11 12:01:05 +0400

    update

--- index.py
+++ index.py
@@ -23,7 +23,39 @@ def strDecrypt(key, word):
         ans += alphabet[(26+(alphabet.find(word[i])-alphabet.find(key[i])))%26]
     return ans
 
+################################################## L I S T #######################################################
+def listEncrypt(key, word):
+    ans = ''
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    key = setKey(key, word)
+    for i in range(len(word)):
+        ans += alphabet[(alphabet.find(word[i])+alphabet.find(key[i]))%26]
+    return ans
 
+def lisetDecrypt(key, word):
+    ans = ''
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    key = setKey(key, word)
+    for i in range(len(word)):
+        ans += alphabet[(26+(alphabet.find(word[i])-alphabet.find(key[i])))%26]
+    return ans
+
+################################################## T U P L E #######################################################
+def tupleEncrypt(key, word):
+    ans = ''
+    alphabet = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+    key = setKey(key, word)
+    for i in range(len(word)):
+        ans += alphabet[(alphabet.find(word[i])+alphabet.find(key[i]))%26]
+    return ans
+
+def tupleDecrypt(key, word):
+    ans = ''
+    alphabet = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+    key = setKey(key, word)
+    for i in range(len(word)):
+        ans += alphabet[(26+(alphabet.find(word[i])-alphabet.find(key[i])))%26]
+    return ans
 
 print(strDecrypt(key, word))
 
